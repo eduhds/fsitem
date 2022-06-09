f9e6dc1ff85e854948beb46ee2e6b239ff7a6398
Eduardo Henrique <eduardohds.dev@outlook.com>
2022-06-09 11:30:33 -0300

    Update linux compatibility

--- dot_ssh_switch/main.m
+++ dot_ssh_switch/main.m
@@ -6,7 +6,7 @@
 //
 
 #import <Foundation/Foundation.h>
-#import "profile_manager.m"
+#import "profile_manager.h"
 
 #define OPT_LIST @"list"
 #define SSH_PROFILE_FOLDER @"/ssh_profiles"
@@ -14,64 +14,65 @@
 
 int main(int argc, const char * argv[]) {
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
-    //@autoreleasepool {
-        if (argc == 1) {
-            printf("*** NO ARGUMENTS ***\n");
-            printf("Use `list` argument to see all profiles or the profile name to change to it.\n");
-        } else {
-            ProfileManager *pManager = [[ProfileManager alloc] init];
+    
+    if (argc == 1) {
+        printf("*** NO ARGUMENTS ***\n");
+        printf("Use `list` argument to see all profiles or the profile name to change to it.\n");
+    } else {
+        ProfileManager *pManager = [[ProfileManager alloc] init];
 
-            NSString *currentPath = [pManager currentPath];
-            NSString *profilesPath = [currentPath stringByAppendingString: SSH_PROFILE_FOLDER];
+        NSString *currentPath = [pManager currentPath];
+        NSString *profilesPath = [currentPath stringByAppendingString: SSH_PROFILE_FOLDER];
+        
+        NSString *arg1 = [NSString stringWithUTF8String: argv[1]];
+     
+        if ([arg1 isEqualTo: OPT_LIST]) {
+            // List SSH Profiles
+            [pManager listProfiles: profilesPath];
+        } else {
+            NSString *profileFolder = [profilesPath stringByAppendingString: [@"/" stringByAppendingString: arg1]];
             
-            NSString *arg1 = [NSString stringWithUTF8String: argv[1]];
-         
-            if ([arg1 isEqualTo: OPT_LIST]) {
-                // List SSH Profiles
-                [pManager listProfiles: profilesPath];
-            } else {
-                NSString *profileFolder = [profilesPath stringByAppendingString: [@"/" stringByAppendingString: arg1]];
+            if (![pManager isProfile: profilesPath argValue: arg1]) {
+                // Profile not exists
+                char create;
                 
-                if (![pManager isProfile: profilesPath argValue: arg1]) {
-                    // Profile not exists
-                    char create;
-                    
-                    printf("*** PROFILE NOT FOUND ***\n");
-                    printf("Create new profile? (y/N)");
-                    scanf("%c", &create);
-                    
-                    // Create new profile
-                    if (create == 'y' || create == 'Y') {
-                        [pManager deleteItem: SSH_FOLDER];
-                        
-                        NSString *profileName = [@"/" stringByAppendingString: arg1];
-                        
-                        bool createSuccess = [pManager createProfile: [profilesPath stringByAppendingString: profileName]];
-                        [pManager deleteItem: SSH_FOLDER];
-                        
-                        bool changedSuccess = [pManager createLink: SSH_FOLDER destination: profileFolder];
-                        
-                        if (createSuccess && changedSuccess) {
-                            printf("*** NEW PROFILE CREATED ***\n");
-                        } else {
-                            printf("*** CREATE FAILED ***\n");
-                        }
-                    }
-                } else {
-                    // Profile exists, change to it
+                printf("*** PROFILE NOT FOUND ***\n");
+                printf("Create new profile? (y/N)");
+                scanf("%c", &create);
+                
+                // Create new profile
+                if (create == 'y' || create == 'Y') {
                     [pManager deleteItem: SSH_FOLDER];
                     
-                    bool created = [pManager createLink: SSH_FOLDER destination: profileFolder];
+                    NSString *profileName = [@"/" stringByAppendingString: arg1];
                     
-                    if (created) {
-                        printf("*** PROFILE CHANGED ***\n");
+                    bool createSuccess = [pManager createProfile: [profilesPath stringByAppendingString: profileName]];
+                    [pManager deleteItem: SSH_FOLDER];
+                    
+                    bool changedSuccess = [pManager createLink: SSH_FOLDER destination: profileFolder];
+                    
+                    if (createSuccess && changedSuccess) {
+                        printf("*** NEW PROFILE CREATED ***\n");
                     } else {
-                        printf("*** CHANGE FAILED ***\n");
+                        printf("*** CREATE FAILED ***\n");
                     }
+                }
+            } else {
+                // Profile exists, change to it
+                [pManager deleteItem: SSH_FOLDER];
+                
+                bool created = [pManager createLink: SSH_FOLDER destination: profileFolder];
+                
+                if (created) {
+                    printf("*** PROFILE CHANGED ***\n");
+                } else {
+                    printf("*** CHANGE FAILED ***\n");
                 }
             }
         }
-    //}
+    }
+
     [pool drain];
+
     return 0;
 }
