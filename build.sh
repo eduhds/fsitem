0583db529d8cf3ef73c3fc66551e5faf74244be2
Eduardo H. da Silva <27dhiouz9@mozmail.com>
2024-08-16 19:12:27 +0000

    chore: update build script

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 mkdir -p bin
 rm bin/*
 
-gcc -c -o bin/termbox2.o fsitem/libs/termbox2.c
+gcc -c -o bin/termbox2.o src/libs/termbox2.c
 
-gcc fsitem/*.m bin/*.o `gnustep-config --objc-flags` \
+gcc src/*.m bin/*.o `gnustep-config --objc-flags` \
     -lobjc -lgnustep-base -std=c11 \
     -o bin/app
